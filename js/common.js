67286838a94e927b236b0b670480bc7926a48619
awcssjs <consultingcanada26@gmail.com>
2026-09-14 19:24:20 +0530

    Add jQuery loading script to common.js

---
+++ js/common.js
@@ -0,0 +1,6 @@
+// Load jQuery from CDN
+const script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.7.1.min.js';
+script.integrity = 'sha256-/JqT3SQfawRcv/BIHPThkB1b7R3JZ6p3xj6q0Y9Q2Q=';
+script.crossOrigin = 'anonymous';
+document.head.appendChild(script);
